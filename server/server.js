ce14b52b0c6ced267c1a47191da5808e970ec8de
Abdelrahman Nasser <abdelrahman.nasseryo@gmail.com>
2025-01-06 09:30:23 +0200

    Build POST & GET Requests and Test With Postman

--- server/server.js
+++ server/server.js
@@ -7,3 +7,22 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+let notes = [];
+
+app.post("/notes", (req, res) => {
+  const body = req.body;
+  console.log("BODY:", body);
+  notes.push(body);
+  console.log("Notes:", notes);
+  res.send(true);
+});
+
+app.get("/notes", (req, res) => {
+  res.send(notes);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server has started on port ${port}...`);
+});
