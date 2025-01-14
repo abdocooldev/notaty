e87948b8bcb538afcd80ac2fbf6df58cf835b1db
Abdelrahman Nasser <abdelrahman.nasseryo@gmail.com>
2025-01-14 15:40:19 +0200

    Display Note Data in Edit Modal & Build Save Edited Note Functionlity

--- client/js/modal-handler.js
+++ client/js/modal-handler.js
@@ -39,12 +39,50 @@ function saveNewNote() {
     })
     .catch((error) => (errEl.innerHTML = error));
 }
-
+// Open Edit Modal Method
+let editNoteModal = document.getElementById("editNoteModal");
 function openEditModal(noteId) {
-  let editNoteModal = document.getElementById("editNoteModal");
+  document.getElementById("editError").innerHTML = "";
   showModal(editNoteModal);
   let closeBtn = document.getElementById("closeEdit");
   let cancelBtn = document.getElementById("cancelEditNoteBtn");
   closeBtn.onclick = () => hideModal(editNoteModal);
   cancelBtn.onclick = () => hideModal(editNoteModal);
+  loadNoteData(noteId);
+}
+// Display Data In Modal
+function loadNoteData(noteId) {
+  editNoteModal.dataset.noteId = noteId;
+  getNoteById(noteId)
+    .then((data) => {
+      document.getElementById("editTitle").value = data.title;
+      document.getElementById("editContent").value = data.content;
+    })
+    .catch((error) => {
+      document.getElementById("editError").innerHTML = error;
+    });
+}
+
+function saveEditNote() {
+  let titleValue = document.getElementById("editTitle").value;
+  let contentValue = document.getElementById("editContent").value;
+  let errEl = document.getElementById("editError");
+  let noteData = {
+    _id: editNoteModal.dataset.noteId,
+    title: titleValue,
+    content: contentValue,
+  };
+  updateNote(noteData)
+    .then((response) => {
+      if (response.ok) {
+        hideModal(editNoteModal);
+        updateNotesTable();
+      } else {
+        response
+          .text()
+          .then((err) => (errEl.innerHTML = err))
+          .catch((error) => console.log(error));
+      }
+    })
+    .catch((error) => (errEl.innerHTML = error));
 }
