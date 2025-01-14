d0230da13285324a47c71e40b0271fd27154561a
Abdelrahman Nasser <abdelrahman.nasseryo@gmail.com>
2025-01-14 14:03:01 +0200

    build Save New Note Method

--- client/js/modal-handler.js
+++ client/js/modal-handler.js
@@ -3,15 +3,39 @@ function showModal(modal) {
   modal.style.display = "block";
 }
 function hideModal(modal) {
-    modal.style.display = "none";
-  }
-//   Open Add Modal Method
-function openAddModal() {
-  let addNoteModal = document.getElementById("addNoteModal");
-  showModal(addNoteModal);
+  modal.style.display = "none";
 }
-// Close Add Modal Method
-let closeBtn = document.getElementById("closeAdd");
-let cancelBtn = document.getElementById("cancelAddNoteBtn");
-closeBtn.onclick = () => hideModal(addNoteModal);
-cancelBtn.onclick = () => hideModal(addNoteModal);
+//   Open Add Modal Method
+let addNoteModal = document.getElementById("addNoteModal");
+function openAddModal() {
+  document.getElementById("addTitle").value = "";
+  document.getElementById("addContent").value = "";
+  document.getElementById("addError").innerHTML = "";
+  showModal(addNoteModal);
+  // Close Add Modal Method
+  let closeBtn = document.getElementById("closeAdd");
+  let cancelBtn = document.getElementById("cancelAddNoteBtn");
+  closeBtn.onclick = () => hideModal(addNoteModal);
+  cancelBtn.onclick = () => hideModal(addNoteModal);
+}
+
+// Save New Note Method
+function saveNewNote() {
+  let titleValue = document.getElementById("addTitle").value;
+  let contentValue = document.getElementById("addContent").value;
+  let errEl = document.getElementById("addError");
+  let noteData = { title: titleValue, content: contentValue };
+  addNote(noteData)
+    .then((response) => {
+      if (response.ok) {
+        hideModal(addNoteModal);
+        updateNotesTable();
+      } else {
+        response
+          .text()
+          .then((err) => (errEl.innerHTML = err))
+          .catch((error) => console.log(error));
+      }
+    })
+    .catch((error) => (errEl.innerHTML = error));
+}
